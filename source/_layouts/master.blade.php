2092bb7acef7d17e53789f5fab707d0c65595835
owlnofeathers <adamjbailey7@gmail.com>
2021-08-11 11:29:41 -0700

    Add social icons to footer

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -66,14 +66,11 @@
         <footer class="bg-white dark:bg-blue-700 text-center text-sm mt-12 py-4" role="contentinfo">
             <ul class="flex flex-col md:flex-row justify-center list-none">
                 <li class="md:mr-2">
-                    &copy; <a href="https://tighten.co" title="Tighten website">Tighten</a> {{ date('Y') }}.
-                </li>
-
-                <li>
-                    Built with <a href="http://jigsaw.tighten.co" title="Jigsaw by Tighten">Jigsaw</a>
-                    and <a href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>.
+                    &copy; <strong>Adam Bailey</strong> {{ date('Y') }}.
                 </li>
             </ul>
+
+            @include('_components.footer-socials')
         </footer>
 
         <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
