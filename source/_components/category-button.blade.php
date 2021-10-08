87ad472bb63756b03886c940baee8ae766094c2f
isAdamBailey <adamjbailey7@gmail.com>
2021-10-08 12:18:19 -0700

    style "you might like" section

--- source/_components/category-button.blade.php
+++ source/_components/category-button.blade.php
@@ -1,5 +1,5 @@
 <a
     href="{{ '/blog/categories/' . $category }}"
     title="View posts in {{ $category }}"
-    class="inline-block bg-gray-300 dark:bg-blue-700 hover:bg-blue-200 dark:hover:bg-blue-800 leading-loose tracking-wide text-gray-800 dark:text-white uppercase text-xs font-semibold rounded mr-4 px-3 pt-px"
+    class="h-6 inline-block bg-gray-300 dark:bg-blue-700 hover:bg-blue-200 dark:hover:bg-blue-800 leading-loose tracking-wide text-gray-800 dark:text-white uppercase text-xs font-semibold rounded mr-4 px-3 pt-px"
 >{{ $category }}</a>
--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -42,8 +42,21 @@
 
     <div class="flex justify-between flex-wrap my-5 mb-20">
         @if ($page->categories[0])
-            <div>
-                You might like other posts in
+            <div class="flex p-5 rounded overflow-hidden shadow-lg dark:bg-gray-700">
+                <svg
+                    class="block mr-3 text-blue-700 dark:text-white"
+                    fill="currentColor"
+                    role="img"
+                    aria-hidden="true"
+                    width="30"
+                    height="30"
+                >
+                    <use
+                        xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xlink:href="/assets/img/icons.svg#icon-comment"
+                    />
+                </svg>
+                <div class="mr-3">You might like other posts in</div>
                 <x-category-button :category="$page->categories[0]"/>
             </div>
         @endif
