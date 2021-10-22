e0bdf727f9edc15ad3b76bcdf6e9e881d2b38189
isAdamBailey <adamjbailey7@gmail.com>
2021-10-22 14:14:13 -0700

    Include a scroll to top button in footer

---
+++ source/_components/scroll-top.blade.php
@@ -0,0 +1,21 @@
+<button
+    class="bg-blue-700 dark:bg-gray-800 hover:bg-blue-900 dark:hover:bg-blue-800 font-semibold text-white py-1 pl-2 pr-4 rounded-full mb-10"
+    onclick="window.scrollTo({top: 0, behavior: 'smooth'});"
+>
+    <div class="flex items-center">
+        <svg
+            class="m-0 p-0 mr-2 transform rotate-180"
+            fill="currentColor"
+            role="img"
+            aria-hidden="true"
+            width="27"
+            height="27"
+        >
+            <use
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                xlink:href="/assets/img/icons.svg#icon-chevron"
+            />
+        </svg>
+        Back To Top
+    </div>
+</button>
