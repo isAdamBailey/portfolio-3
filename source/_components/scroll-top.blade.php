9766ded6585e573515b0c177967b9333d0cb6a33
Adam Bailey <adamjbailey7@gmail.com>
2022-01-06 07:41:18 -0800

    improve scrolltop component

--- source/_components/scroll-top.blade.php
+++ source/_components/scroll-top.blade.php
@@ -1,21 +1,49 @@
-<button
-    class="bg-blue-700 dark:bg-gray-800 hover:bg-blue-900 dark:hover:bg-blue-800 font-semibold text-white py-1 pl-2 pr-4 rounded-full mb-10"
-    onclick="window.scrollTo({top: 0, behavior: 'smooth'});"
+<div
+    id="scrollTopButton"
+    class="invisible sticky w-full flex justify-end bottom-0 pb-3 pr-5 lg:pr-16 transition"
 >
-    <div class="flex items-center">
-        <svg
-            class="m-0 p-0 mr-2 transform rotate-180"
-            fill="currentColor"
-            role="img"
-            aria-hidden="true"
-            width="27"
-            height="27"
+    <div
+        class="bg-blue-300 bg-opacity-75 rounded-full cursor-pointer text-gray-800 hover:text-white transition"
+    >
+        <button
+            role="button"
+            aria-label="scroll to top of the page"
+            onclick="actions.scrollToTop()"
         >
-            <use
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                xlink:href="/assets/img/icons.svg#icon-chevron"
-            />
-        </svg>
-        Back To Top
+            <svg
+                class="m-0 p-0 mr-2 transform rotate-180"
+                fill="currentColor"
+                role="img"
+                aria-hidden="true"
+                width="27"
+                height="27"
+            >
+                <use
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    xlink:href="/assets/img/icons.svg#icon-chevron"
+                />
+            </svg>
+        </button>
     </div>
-</button>
+</div>
+
+@push('scripts')
+<script>
+    const actions = {
+        scrollToTop() {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        },
+        handleScroll() {
+            const scrollBtn = document.getElementById('scrollTopButton');
+
+            if (window.scrollY > 0) {
+                scrollBtn.classList.remove("invisible");
+            } else {
+                scrollBtn.classList.add("invisible");
+            }
+        },
+    }
+
+    window.addEventListener("scroll", actions.handleScroll);
+</script>
+@endpush
--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -79,9 +79,8 @@
             @yield('body')
         </main>
 
+        <x-scroll-top/>
         <footer class="bg-white dark:bg-blue-700 text-center text-sm mt-12 py-4" role="contentinfo">
-            <x-scroll-top/>
-
             <ul class="flex flex-col md:flex-row justify-center list-none">
                 <li class="md:mr-2">
                     &copy; <strong>Adam Bailey</strong> {{ date('Y') }}.
